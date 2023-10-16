8b1ea8038f5cbb29020093bc2b92abfad6c83df1
Dell-User <Dell-User@DESKTOP-4LNHG9G>
2023-10-16 12:35:38 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum_of_number_if_same_numbers_triple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numerical values:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            int sum = num1 + num2;
+
+            if (num1 == num2)
+            {
+                sum *= 3;
+
+            }
+
+            Console.WriteLine("The computed sum is: " + sum);
+            Console.ReadLine(); 
+        }
+    }
+}
